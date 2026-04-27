747c9bae5e0ab322f4d283f8e6a631ea8d544aa4
AfinoviczDev <jpafinovicz@gmail.com>
2026-04-27 14:21:42 -0300

    first commit

---
+++ src/main/java/dev/afinovicz/AfinoviczCommerce/AfinoviczCommerceApplication.java
@@ -0,0 +1,13 @@
+package dev.afinovicz.AfinoviczCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfinoviczCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfinoviczCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/afinovicz/AfinoviczCommerce/AfinoviczCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.afinovicz.AfinoviczCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfinoviczCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
