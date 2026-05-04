d4453845bbed3e6d7b1061a81bf45f856dce5854
AfinoviczDev <jpafinovicz@gmail.com>
2026-05-04 19:25:10 -0300

    feat: criação de busca paginada dos produtos

--- src/main/java/dev/afinovicz/AfinoviczCommerce/controller/ProductController.java
+++ src/main/java/dev/afinovicz/AfinoviczCommerce/controller/ProductController.java
@@ -1,15 +1,15 @@
 package dev.afinovicz.AfinoviczCommerce.controller;
 
 import dev.afinovicz.AfinoviczCommerce.dto.ProductDTO;
-import dev.afinovicz.AfinoviczCommerce.entities.Product;
-import dev.afinovicz.AfinoviczCommerce.repositories.ProductRepository;
 import dev.afinovicz.AfinoviczCommerce.services.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -26,5 +26,10 @@ public class ProductController {
     public ProductDTO findById(@PathVariable Long id) {
         ProductDTO dto = service.findById(id);
         return dto;
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
--- src/main/java/dev/afinovicz/AfinoviczCommerce/services/ProductService.java
+++ src/main/java/dev/afinovicz/AfinoviczCommerce/services/ProductService.java
@@ -3,9 +3,12 @@ package dev.afinovicz.AfinoviczCommerce.services;
 import dev.afinovicz.AfinoviczCommerce.dto.ProductDTO;
 import dev.afinovicz.AfinoviczCommerce.entities.Product;
 import dev.afinovicz.AfinoviczCommerce.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,5 +26,12 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO>  findAll(Pageable pageable) {
+        Page<Product>  result = repository.findAll(pageable);
+        //CONVERTER PRODUCT DA LISTA PARA PRODUCTDTO
+        return result.map(x -> new ProductDTO(x));
     }
 }
